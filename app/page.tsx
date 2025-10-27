6f1042f0e5d7ec398c3d5a1b900dfbbfafe2f694
Roshan <godxrosu@gmail.com>
2025-10-27 19:43:54 +0530

    studied useref and usestate added tetx inptut tommr i need check that save data into locallaly and list view builder i need check

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import Card from "@/componets/card";
 import Card2 from "@/componets/card2";
 import ProfileCard from '@/componets/porfileCard' ; 
+import Input from "@/componets/input";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen" >
+    <div className="justify-center items-center h-screen" >
       <ProfileCard/>
+      <Input/>
        
     </div>
   );
---
+++ componets/input.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useRef , useState } from "react";
+
+
+export default function Input(){
+    const inputRef = useRef(null);
+    const [textdata , setdata]= useState<string>('');
+    const inout2Ref=useRef<HTMLInputElement | null>(null);
+   function handClick(){
+    console.log('clicked');
+    console.log(inout2Ref.current?.value);
+
+   }
+
+    const arrowFunction=():void=>{
+        console.log('arrow function called');
+         console.log(inout2Ref.current?.value);
+         setdata(inout2Ref.current?.value??'no data');
+    }
+      
+    return(<><input type="text" ref={inout2Ref} placeholder='Enter name ' className="w-200 h-40 bg-amber-50 m-10 pl-4 "/>
+    
+    <button onClick={ arrowFunction} className="bg-amber-300 w-80 h-40 text-xl text-white shadow-2xl rounded-2xl ">Save</button>
+    <p>{textdata}</p>
+    </>)
+}
