cf742f5a6b9b7500a20a89450df22f1bf043a462
Roshan <godxrosu@gmail.com>
2025-11-06 11:28:57 +0530

    added with and leaned with react query as basic now i need deep into that react hook and

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import Card from "@/componets/card";
 import Card2 from "@/componets/card2";
@@ -7,11 +9,59 @@ import Page from "@/componets/page";
 import { Button } from "@/components/ui/button";
 import { InputDemo } from "@/componets/shandcn";
 import DictionarySearch from "@/componets/DictionarySearch";
+import { useQuery, useMutation } from "@tanstack/react-query";
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+async function fetchPost(): Promise<Post[]> {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json(); // ✅ wait for JSON to resolve
+
+  console.log("Output JSON is:", data); // ✅ actual data logged
+  return data;
+}
 
 export default function Home() {
+  const { data, error, isLoading, isError } = useQuery({
+    queryKey: ["poast"],
+    queryFn: fetchPost,
+  });
+
+  if (isLoading)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <p className="text-[24px] font-semibold text-gray-700">Loading...</p>
+      </div>
+    );
+
+  if (isError)
+    return (
+      <p className="text-red-500 font-bold   text-[40px]">
+        Error:{(error as Error).message}
+      </p>
+    );
+
   return (
-       <div>
-      <DictionarySearch />
+    <div className="flex  flex-col items-center justify-center">
+      {data?.map((post, key) => (
+        <div
+          key={key}
+          className="
+    p-4 w-[450px] mb-4 rounded-2xl border border-gray-300
+    bg-gradient-to-r from-amber-500 to-pink-500 
+    transition-all duration-300 ease-in-out
+    hover:border-white hover:w-[460px]
+  "
+        >
+          {" "}
+          <h2 className="text-[20px] font-weight: 600 ">title:{post.title}</h2>
+          <p>dis:{post.userId}</p> <p>body:{post.body}</p>
+        </div>
+      ))}
     </div>
   );
 }
---
+++ componets/queryClient.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactNode } from "react";
+import { useState } from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function ReactQueryProvider({ children }: Props) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
